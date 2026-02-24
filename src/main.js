1d55509444de0378461481cdb463d33bf9ef6348
Artem Bagno <bgn.rtm@gmail.com>
2026-02-24 23:30:56 +0200

    after check by mentor

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import { fetchFeetbacks } from './js/api/feedback-api';
 import { createFeedbackList } from '../src/js/feedback/feedbacks';
+import { examplTost } from './js/components/special-toast';
 import '../src/js/feedback/feedbacks';
 import './js/artist/render-artist';
 import './js/close-open.js/mobile-menu';
 import './js/components/modal-artist/modal';
 import './js/feedback/feedback-modal';
 
-
-fetchFeetbacks().then(createFeedbackList);
+fetchFeetbacks().then(createFeedbackList).catch(examplTost);
