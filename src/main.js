d18c687eb983ad13ac1e0c1a78d1d1ce595d0789
vjosik <108922728+vjosik@users.noreply.github.com>
2026-02-24 16:38:29 +0200

    svg fix

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-
 import { fetchFeetbacks } from './js/api/feedback-api';
 import { createFeedbackList } from '../src/js/feedback/feedbacks';
 import '../src/js/feedback/feedbacks';
@@ -6,5 +5,6 @@ import './js/artist/render-artist';
 import './js/close-open.js/mobile-menu';
 import './js/components/modal-artist/modal';
 import './js/feedback/feedback-modal';
+import '/assets/icons/sprite.svg';
 
 fetchFeetbacks().then(createFeedbackList);
